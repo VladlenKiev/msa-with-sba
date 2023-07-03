01b6d51394af20e37f018c16d0cfbc1afab53759
Volodymyr Mankivskyi <volodymyr.mankivskyi.external@cloudpay.net>
2023-07-03 10:45:30 +0200

    init commit

--- spring-boot-admin/src/main/java/com/training/msademo/SpringBootAdminApplication.java
+++ spring-boot-admin/src/main/java/com/training/msademo/SpringBootAdminApplication.java
@@ -6,11 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.web.server.ServerHttpSecurity;
-//import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-//import org.springframework.security.web.server.SecurityWebFilterChain;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 
 @EnableAdminServer
@@ -18,16 +17,16 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 //@EnableDiscoveryClient
 public class SpringBootAdminApplication {
-//    private final String adminContextPath;
-
-//    public SpringBootAdminApplication(AdminServerProperties adminServerProperties) {
-//        this.adminContextPath = adminServerProperties.getContextPath();
-//    }
+    private final String adminContextPath;
+//
+    public SpringBootAdminApplication(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAdminApplication.class, args);
     }
 
-    /*@Bean
+    @Bean
     public SecurityWebFilterChain securityWebFilterChainSecure(ServerHttpSecurity http) {
         SavedRequestAwareAuthenticationSuccessHandler successHandler =
                 new SavedRequestAwareAuthenticationSuccessHandler();
@@ -43,30 +42,6 @@ public class SpringBootAdminApplication {
                 .and().httpBasic()
                 .and().csrf().disable().build();
 
-    }*/
-
-//    @Configuration
-//    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            // Page with login form is served as /login.html and does a POST on /login
-//            http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll
-//            // The UI does a POST on /logout on logout
-//            http.logout().logoutUrl("/logout");
-//            // The ui currently doesn't support csrf
-//            http.csrf().disable();
-//
-//            // Requests for the login page and the static assets are allowed
-//            http.authorizeRequests()
-//                    .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
-//                    .permitAll();
-//            // ... and any other request needs to be authorized
-//            http.authorizeRequests().antMatchers("/**").authenticated();
-//
-//            // Enable so that the clients can authenticate via HTTP basic for registering
-//            http.httpBasic();
-//        }
-//    }
-//     end::configuration-spring-security[]
+    }
 
 }
--- srvB/src/main/java/com/msa/srvb/ApplicationSrvB.java
+++ srvB/src/main/java/com/msa/srvb/ApplicationSrvB.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,9 +16,9 @@ public class ApplicationSrvB {
 		SpringApplication.run(ApplicationSrvB.class, args);
 	}
 
-//	@Bean
-//	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//		return http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests.anyRequest().permitAll())
-//				.csrf().disable().build();
-//	}
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests.anyRequest().permitAll())
+				.csrf().disable().build();
+	}
 }
